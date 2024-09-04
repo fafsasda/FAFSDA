27169f4b29346fa65465a634723d17f13b2c451f
root <fafsasda@outlook.com>
2024-09-04 14:08:28 +0000

    feat: initiated the project

--- scripts/proxy/utils/deployed-address.ts
+++ scripts/proxy/utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xaCF84CED4eedD053C0417082F09bf2bB2d45899E'
-export const ProxyAdmin = '0xC72182487A5D82aD21F26B02900b9B7265442355'
-export const SWTRImplementation = '0xE75eD02BA12089212471dB9CB22AdFa577ea2Af5'
+export const SWTRProxy = '0xb786E109aa40311Efe54862D2B8Bc6d99c2e61EB'
+export const ProxyAdmin = '0x3CeB0daE2fE047279b5B6F0356D2A3c22E9f6ABf'
+export const SWTRImplementation = '0xFfC77a4C961922500Fb91EF8Ca61653E7578c8ec'
     
